ef9632632f41d0b1ba62d662275747d1f8d6dfe4
Egor Ukolov <ukolovegor@yandex.ru>
2023-09-09 22:15:17 +0700

    Проработаны модели AnimalType и Question

--- PersonalQuiz/Models/Question.swift
+++ PersonalQuiz/Models/Question.swift
@@ -10,3 +10,40 @@ struct Question {
     let type: ResponseType
     let answers: [Answer]
 }
+
+extension Question {
+    static func getQuestions() -> [Question] {
+        return [
+            Question(
+                text: "Какую пищу вы препочитаете?",
+                type: .single,
+                answers: [
+                    Answer(text: "Стейк", type: .dog),
+                    Answer(text: "Рыба", type: .cat),
+                    Answer(text: "Морковь", type: .rabbit),
+                    Answer(text: "Кукуруза", type: .turtle)
+                ]
+            ),
+            Question(
+                text: "Что вам нравится больше?",
+                type: .multiple,
+                answers: [
+                    Answer(text: "Плавать", type: .dog),
+                    Answer(text: "Спать", type: .cat),
+                    Answer(text: "Обниматься", type: .rabbit),
+                    Answer(text: "Есть", type: .turtle)
+                ]
+            ),
+            Question(
+                text: "Любите ли вы поездки на машине?",
+                type: .ranged,
+                answers: [
+                    Answer(text: "Ненавижу", type: .dog),
+                    Answer(text: "", type: .cat),
+                    Answer(text: "", type: .rabbit),
+                    Answer(text: "Обожаю", type: .turtle)
+                ]
+            ),
+        ]
+    }
+}
